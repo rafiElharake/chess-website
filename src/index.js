11a618888e5f693e0705245ae834362d2249c90b
crimson32 <rafi_harake@hotmail.com>
2024-04-26 11:29:16 +0300

    Your commit message here

--- src/index.js
+++ src/index.js
@@ -1,13 +1,61 @@
 import React from 'react';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createRoot } from 'react-dom/client';
+import App from './App';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Login from './components/Login/Login';
+import Signup from './components/Login/Signup';
+import Play from './playy/Home';
+import GameApp from './playy/GameApp';
+import Home from './components/home/Home';
+import Analysis from './components/Analysis/Analysis';
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import ReactDOM from 'react-dom';
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        path: "/analysis",
+        element: <Analysis />,
+      },
+      {
+        path: "/play",
+        element: <Play />,
+      },      {
+        path: "/game/:id",
+        element: <DndProvider backend={HTML5Backend}> <GameApp/></DndProvider>,
+      },      {
+        path: "/login",
+        element: <Login />,
+      },      {
+        path: "/signup",
+        element: <Signup />,
+      },
+    ],
+  },
+]);
+
+  ReactDOM.createRoot(document.getElementById("root")).render(
+   <div> 
+     <Router>
+        <Routes> 
+        <Route path="/" element={<Home/>} />
+        <Route path="/analysis" element={<Analysis/>} />
+        <Route path="/play" element={<Play/>} />
+        <Route path="/game/:id" element={<DndProvider backend={HTML5Backend}> <GameApp/></DndProvider>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
+        </Routes>
+    </Router>
+    
+    </div>
   );
+
 reportWebVitals();
